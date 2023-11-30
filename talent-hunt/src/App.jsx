00a4fc09a7b2d6811cacfc373a69d83cd66fc83e
Promise <promiseezema11@gmail.com>
2023-11-30 12:34:01 +0100

    updated

--- talent-hunt/src/App.jsx
+++ talent-hunt/src/App.jsx
@@ -2,19 +2,12 @@ import './App.css'
 import Talents from './Routes/Talents'
 import Home from './components/Home'
 
-
-
-
 function App() {
-  
-
   return (
     <>
       <div className="App_container">
         <Home />
         <Talents />
-        
-      
       </div>
     </>
   )

3b408e53f7f6e7cb920e74be7c5fdf1fe0cd550e
unknown <eunzyfads@gmail.com>
2023-12-07 14:45:15 +0100

    corrections of landing page

--- talent-hunt/src/App.jsx
+++ talent-hunt/src/App.jsx
@@ -4,16 +4,19 @@ import ContactUs from './Routes/ContactUs'
 import Talents from './Routes/Talents'
 import Home from './components/frontend/Home'
 import Footer from './components/frontend/Footer'
-
-
-
-
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Register from './components/auth/register'
 
 
 function App() {
   return (
     <>
+    <Router>
+      <Switch>
+        <Route path="/register" exact component={<Register />} />
+      </Switch>
+    </Router>
+    
       <div className="App_container">
         <Home />
         <section id="talents"><Talents /></section>
---
+++ talent-hunt/src/components/auth/register.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Register = () => {
+  return (
+    <div className="register">
+        <div className="register-container">
+            <div className="register-img">
+                
+            </div>
+            <div>
+                <h1>IVAV TALENTS</h1>
+                <p>Create an Account</p>
+                <div className="form">
+                    <input type="name" placeholder="Full Name" />
+                    
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
